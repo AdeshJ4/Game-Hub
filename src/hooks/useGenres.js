f592d0238aef671cd30b6ef66e2d1d8e3b3c16aa
AdeshJ4 <adejad1234@gmail.com>
2023-11-01 12:21:53 +0530

    Handle games with no Image

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -16,36 +16,3 @@ export default useGenres;
 
 
 
-
-
-// import { useState, useEffect } from 'react';
-// import apiClient from '../services/api-client';
-// import { CanceledError } from 'axios';
-
-// const useGenres = () => {
-//     const [genres, setGenres] = useState([]);
-//     const [error, setError] = useState("");
-//     const [isLoading, setLoading] = useState(false);
-
-//     useEffect(()=>{
-//         const controller = new AbortController();
-//         setLoading(true);
-//         apiClient
-//             .get("/genres", {signal: controller.signal})
-//             .then((res)=>{
-//                 setGenres(res.data.results); // result is array => [id, name, slug, games_count, image_background]
-//                 setLoading(false);
-//             })
-//             .catch((err)=>{
-//                 if(err instanceof CanceledError) return;
-//                 setError(err.message);
-//                 setLoading(false);
-//             });
-
-//             return ()=> controller.abort();
-//     }, []);
-
-//     return {genres, error, isLoading}
-// }
-
-// export default useGenres;
--- src/services/image-url.js
+++ src/services/image-url.js
@@ -1,3 +1,5 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url) => {
     //input : "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg";
     //output : https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg;
@@ -6,7 +8,7 @@ const getCroppedImageUrl = (url) => {
         TypeError: Cannot read properties of null (reading 'indexOf') at getCroppedImageUrl (image-url.js:6:23)"
     
     */
-    if(!url) return ''; 
+    if(!url) return noImage; 
 
     const target = 'media/';
     const index = url.indexOf(target) + target.length // 22(m)+6 = 28 (g)
