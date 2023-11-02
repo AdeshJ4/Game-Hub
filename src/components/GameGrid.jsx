6855c9ac9f0a88453d4c77a49382f0aadb2bd648
AdeshJ4 <adejad1234@gmail.com>
2023-11-02 12:38:50 +0530

    Refactor GameGrid

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -8,9 +8,10 @@ const GameGrid = ({gameQuery}) => {
   const { data, error, isLoading } = useGames(gameQuery); // custom hook
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; // it represent 2 skeleton card component
 
+  if(error) return  <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
+
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={6}
@@ -28,7 +29,6 @@ const GameGrid = ({gameQuery}) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
